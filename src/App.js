f7f56aa7bce9875570b91e6cf03e37f2fe420e6f
phenopreneur <54966250+phenopreneur@users.noreply.github.com>
2022-09-29 19:40:22 +0100

    second

--- src/App.js
+++ src/App.js
@@ -5,8 +5,50 @@ import Navigation from './components/Navigation';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Main from './pages/Main';
+import scrollreveal from 'scrollreveal'
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 2000,
+      reset: true
+    });
+    sr.reveal(
+      `
+      .item
+      `,
+      {
+        opacity: 0,
+        interval: 300
+      }
+    )
+  })
+
+  useEffect(() => {
+    const srl = scrollreveal({
+      origin: "left",
+      distance: "50px",
+      duration: 2000,
+      reset: true
+    });
+    srl.reveal(
+      `
+      .home,
+      .question,
+      .engine,
+      .footer-container
+      `,
+      {
+        opacity: 0,
+        interval: 300
+      }
+    )
+    })
+  
   return (
     <>
       <Navigation />
